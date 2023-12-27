0896a4f9ba4d3511df6728e2946dac624c307ea6
David B. Maier <davidbmaier@gmail.com>
2023-12-27 12:05:18 +0100

    Add initial JWT setup

--- api/go.mod
+++ api/go.mod
@@ -3,8 +3,15 @@ module github.com/davidbmaier/dbm-api
 go 1.21.4
 
 require (
+	github.com/gofiber/contrib/jwt v1.0.8
+	github.com/gofiber/fiber/v2 v2.51.0
+	github.com/golang-jwt/jwt/v5 v5.2.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/andybalholm/brotli v1.0.6 // indirect
-	github.com/gofiber/fiber/v2 v2.51.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- api/main.go
+++ api/main.go
@@ -1,13 +1,87 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+
+	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/golang-jwt/jwt/v5"
+
+	"github.com/joho/godotenv"
+)
+
+var env map[string]string
 
 func main() {
-	app := fiber.New()
+	var err error
+	env, err = godotenv.Read(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+	app := fiber.New(fiber.Config{
+		Prefork:       false,
+		CaseSensitive: true,
+		StrictRouting: false,
+		ServerHeader:  "",
+		AppName:       "DBM API",
 	})
 
-	app.Listen(":3000")
+	// Login route
+	app.Post("/login", login)
+
+	// Unauthenticated route
+	app.Get("/", accessible)
+
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte(env["JWT_SECRET"])},
+	}))
+
+	// Restricted Routes
+	app.Get("/restricted", restricted)
+
+	app.Listen(fmt.Sprintf(":%s", env["PORT"]))
+}
+
+func login(c *fiber.Ctx) error {
+	user := c.FormValue("user")
+	pass := c.FormValue("pass")
+
+	// Throws Unauthorized error
+	if user != env["ADMIN_USER"] || pass != env["ADMIN_PASS"] {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	// Create the Claims
+	claims := jwt.MapClaims{
+		"name":  "Admin",
+		"admin": true,
+		"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	// Create token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte(env["JWT_SECRET"]))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": t})
+}
+
+func accessible(c *fiber.Ctx) error {
+	return c.SendString("Accessible")
+}
+
+func restricted(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.SendString("Welcome " + name)
 }
