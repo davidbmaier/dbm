a693f6a704adeff2e9b7f104c1dae7522f26910f
David B. Maier <davidbmaier@gmail.com>
2024-01-04 15:12:21 +0100

    Clean up color scheme and login

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -12,16 +12,20 @@
 	{#if !$isLoading}
 		<Navbar>
 			<NavBrand href="/works">
+				<img src="/src/images/logo.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
 				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
 					{$_('layout.title')}
 				</span>
 			</NavBrand>
 			<NavHamburger />
-			<NavUl {activeUrl} activeClass="nav-active">
-				<NavLi href="/works">{$_('layout.works.label')}</NavLi>
-				<NavLi href="/artists">{$_('layout.artists.label')}</NavLi>
-			</NavUl>
+			<span style={activeUrl != '/' ? '' : 'visibility:hidden'}>
+				<NavUl {activeUrl} activeClass="nav-active">
+					<NavLi href="/works">{$_('layout.works.label')}</NavLi>
+					<NavLi href="/artists">{$_('layout.artists.label')}</NavLi>
+				</NavUl>
+			</span>
 		</Navbar>
+		<hr />
 
 		<main id="content-wrapper">
 			<slot />
@@ -33,6 +37,7 @@
 	#wrapper {
 		height: 100vh;
 	}
+
 	#content-wrapper {
 		height: calc(100% - 72px);
 		padding: 20px 50px;
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -4,24 +4,24 @@
 	import { login } from '$lib/requests';
 	import Error from '$lib/components/Error.svelte';
 
-	let email = '';
+	let user = '';
 	let password = '';
 	let error = '';
 
 	const performLogin = async () => {
-		const loginResponse = await login(email, password);
+		const loginResponse = await login(user, password);
 		error = loginResponse?.error || '';
 	};
 </script>
 
 <section id="login-wrapper">
 	<div>
-		{$_('login.email')}
-		<Input bind:value={email} />
+		{$_('login.user')}
+		<Input bind:value={user} />
 	</div>
 	<div>
 		{$_('login.password')}
-		<Input bind:value={password} />
+		<Input type="password" bind:value={password} />
 	</div>
 	<div id="login-button">
 		<Button on:click={performLogin}>{$_('login.login')}</Button>
@@ -37,7 +37,7 @@
 	#login-wrapper {
 		max-width: 500px;
 		margin: auto;
-		margin-top: 50px;
+		margin-top: 150px;
 	}
 	#login-button {
 		margin-top: 10px;
